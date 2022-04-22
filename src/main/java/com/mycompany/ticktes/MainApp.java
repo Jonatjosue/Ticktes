df4455ec4dc6aa59c8d1ec755c6ac10abd664baa
Jonathan <84944284+Jonatjosue@users.noreply.github.com>
2022-04-22 11:40:16 -0600

    Today 22

--- src/main/java/com/mycompany/ticktes/MainApp.java
+++ src/main/java/com/mycompany/ticktes/MainApp.java
@@ -1,10 +1,17 @@
-package com.mycompany.ticktes;
+    package com.mycompany.ticktes;
 import java.util.ArrayList;
 public class MainApp {
     
     public static ArrayList<ClassForUsers> Users;
     
     public static ArrayList<TeamMembersClass>Tusers;
+    
+    public static ArrayList<TeamLeaderClass>Lusers;
+    
+    public static ArrayList<TeamsNames>TNames;
+    
+    static String CurrentPasswordAdmin;
+    
     
     public static void main(String[] args) {
         Login L= new Login();
@@ -13,6 +20,13 @@ public class MainApp {
         
         Tusers= new ArrayList<>();
         
+        Lusers= new ArrayList<>();
+        
+        TNames= new ArrayList<>();
+        
+        
+        
     }
+
     
 }
