ed13b53a5f49edcd3d360b3a73f33b6ff260a1e2
Jonathan <84944284+Jonatjosue@users.noreply.github.com>
2022-04-22 11:36:15 -0600

    Today 22

--- src/main/java/com/mycompany/ticktes/TeamMembersClass.java
+++ src/main/java/com/mycompany/ticktes/TeamMembersClass.java
@@ -1,8 +1,107 @@
 package com.mycompany.ticktes;
 public class TeamMembersClass {
     
+    
     private String Tuser;
     private String Tpassword;
+    private  String Tuserlastname;
+    private  String Temail;
+    private String Tage;
+    private int TeamNumber;
+    private String TeamName;
+    private boolean BelongToTeam;
+    
+    /**
+     * @return the BelongToTeam
+     */
+    public boolean getBelongToTeam() {
+        return BelongToTeam;
+    }
+
+    /**
+     * @param BelongToTeam the BelongToTeam to set
+     */
+    public void setBelongToTeam(boolean BelongToTeam) {
+        this.BelongToTeam = BelongToTeam;
+    }
+
+    /**
+     * @return the TeamName
+     */
+    public String getTeamName() {
+        return TeamName;
+    }
+
+    /**
+     * @param TeamName the TeamName to set
+     */
+    public void setTeamName(String TeamName) {
+        this.TeamName = TeamName;
+    }
+
+     
+    
+    /**
+     * @return the Team
+     */
+    public int getTeamNumber() {
+        return TeamNumber;
+    }
+
+    /**
+     * @param TeamNumber the TeamNumber to set
+     */
+    public void setTeamNumber(int TeamNumber) {
+        this.TeamNumber = TeamNumber;
+    }
+
+    
+   
+    /**
+     * @return the Tage
+     */
+    public String getTage() {
+        return Tage;
+    }
+
+    /**
+     * @param Tage the Tage to set
+     */
+    public void setTage(String Tage) {
+        this.Tage = Tage;
+    }
+    
+    
+
+    /**
+     * @return the Tuserlastname
+     */
+    public String getTuserlastname() {
+        return Tuserlastname;
+    }
+
+    /**
+     * @param Tuserlastname the Tuserlastname to set
+     */
+    public void setTuserlastname(String Tuserlastname) {
+        this.Tuserlastname = Tuserlastname;
+    }
+
+    /**
+     * @return the Temail
+     */
+    public String getTemail() {
+        return Temail;
+    }
+
+    /**
+     * @param Temail the Temail to set
+     */
+    public void setTemail(String Temail) {
+        this.Temail = Temail;
+    }
+    
+   
 
     /**
      * @return the Tusers
