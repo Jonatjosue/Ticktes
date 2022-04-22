df0c677fe510d42e4c9da0f4477622a484abc952
Jonathan <84944284+Jonatjosue@users.noreply.github.com>
2022-04-22 11:49:06 -0600

    Today 22

---
+++ src/main/java/com/mycompany/ticktes/TeamsNames.java
@@ -0,0 +1,38 @@
+package com.mycompany.ticktes;
+public class TeamsNames {
+    
+    private String NameofTeam;
+    private int TeamNumberPosition;
+    
+
+    /**
+     * @return the NameofTeam
+     */
+    public String getNameofTeam() {
+        return NameofTeam;
+    }
+
+    /**
+     * @param NameofTeam the NameofTeam to set
+     */
+    public void setNameofTeam(String NameofTeam) {
+        this.NameofTeam = NameofTeam;
+    }
+
+    /**
+     * @return the TeamNumberPosition
+     */
+    public int getTeamNumberPosition() {
+        return TeamNumberPosition;
+    }
+
+    /**
+     * @param TeamNumberPosition the TeamNumberPosition to set
+     */
+    public void setTeamNumberPosition(int TeamNumberPosition) {
+        this.TeamNumberPosition = TeamNumberPosition;
+    }
+    
+    
+    
+}
