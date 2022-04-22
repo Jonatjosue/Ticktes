bf7c58a3cbdcf74bd5c3bc4db5d179e573e2d6e4
Jonathan <84944284+Jonatjosue@users.noreply.github.com>
2022-04-22 11:48:06 -0600

    Today 22

---
+++ src/main/java/com/mycompany/ticktes/TeamLeaderClass.java
@@ -0,0 +1,116 @@
+package com.mycompany.ticktes;
+
+public class TeamLeaderClass {
+
+    
+    
+    private String Luser;
+    private String Lpassword;
+    private String Luserlastname;
+    private String Lemail;
+    private String Ldeparment;
+    private boolean Belong;
+    private int TeamNumber;
+    /**
+     * @return the TeamNumber
+     */
+    public int getTeamNumber() {
+        return TeamNumber;
+    }
+
+    /**
+     * @param TeamNumber the TeamNumber to set
+     */
+    public void setTeamNumber(int TeamNumber) {
+        this.TeamNumber = TeamNumber;
+    }
+     
+    /**
+     * @return the Ldeparment
+     */
+    public String getLdeparment() {
+        return Ldeparment;
+    }
+
+    /**
+     * @param Ldeparment the Ldeparment to set
+     */
+    public void setLdeparment(String Ldeparment) {
+        this.Ldeparment = Ldeparment;
+    }
+    
+   
+
+    /**
+     * @return the Luserlastname
+     */
+    public String getLuserlastname() {
+        return Luserlastname;
+    }
+
+    /**
+     * @param Luserlastname the Luserlastname to set
+     */
+    public void setLuserlastname(String Luserlastname) {
+        this.Luserlastname = Luserlastname;
+    }
+
+    /**
+     * @return the Lemail
+     */
+    public String getLemail() {
+        return Lemail;
+    }
+
+    /**
+     * @param Lemail the Lemail to set
+     */
+    public void setLemail(String Lemail) {
+        this.Lemail = Lemail;
+    }
+    
+
+
+    /**
+     * @return the Luser
+     */
+    public String getLuser() {
+        return Luser;
+    }
+
+    /**
+     * @param Luser the Luser to set
+     */
+    public void setLuser(String Luser) {
+        this.Luser = Luser;
+    }
+
+    /**
+     * @return the Lpassword
+     */
+    public String getLpassword() {
+        return Lpassword;
+    }
+
+    /**
+     * @param Lpassword the Lpassword to set
+     */
+    public void setLpassword(String Lpassword) {
+        this.Lpassword = Lpassword;
+    }
+
+    /**
+     * @return the Belong
+     */
+    public boolean getBelong() {
+        return Belong;
+    }
+
+    /**
+     * @param Belong the Belong to set
+     */
+    public void setBelong(boolean Belong) {
+        this.Belong = Belong;
+    }
+    
+}
